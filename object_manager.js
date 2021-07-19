f7a0f3829442c28bbb37832bad323a7c83da6457
SweeetNik <59518348+SweeetNik@users.noreply.github.com>
2021-07-19 14:13:57 +0300

    Update object_manager.js

--- object_manager.js
+++ object_manager.js
@@ -1,12 +1,11 @@
 ymaps.ready(init);
 
 function init () {
-    var geolocation = ymaps.geolocation;
+    var geolocation = ymaps.geolocation;    // получаем данные о геолокации
     geolocation.get({
         provider: 'browser',
         mapStateAutoApply: true
-    }).then(function (result) {
-        var gloc = ymaps.geolocation.get();
+    }).then(function (result) { // если геолокация определена
         var mapContainer = $('#map');
         bounds = result.geoObjects.get(0).properties.get('boundedBy');
         mapState = ymaps.util.bounds.getCenterAndZoom(
@@ -14,10 +13,10 @@ function init () {
                 [mapContainer.width(), mapContainer.height()]
 
             );
-    var myMap = new ymaps.Map('map', mapState, {
+    var myMap = new ymaps.Map('map', mapState, {    // создание карты
             searchControlProvider: 'yandex#search'
         }),
-        objectManager = new ymaps.ObjectManager({
+        objectManager = new ymaps.ObjectManager({   // создание objectManager
             // Чтобы метки начали кластеризоваться, выставляем опцию.
             clusterize: true,
             // ObjectManager принимает те же опции, что и кластеризатор.
@@ -32,10 +31,34 @@ function init () {
     objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
     myMap.geoObjects.add(objectManager);
 
+    $.ajax({    // выгрузка данных из data.json
+        url: "data.json"
+    }).done(function(data) {
+        data.features.forEach(each => each.properties.balloonContentBody = "<input type=\"button\" value=\"Выбрать\" id=\"button1\" onclick=\"console.log(document.getElementsByClassName('ymaps-2-1-78-balloon-content__header')[0].innerHTML); document.getElementById('map').style.display = 'none'\">");
+        objectManager.add(data);
+    });
+},
+    function(err) { // если геолокация неопределена браузером
+    var myMap = new ymaps.Map('map', {
+    center: [55.753220, 37.622513], // координаты Москвы
+    zoom: 10
+}, {
+    searchControlProvider: 'yandex#search'
+}),
+    objectManager = new ymaps.ObjectManager({
+            clusterize: true,
+            clusterHasBalloon: false,
+            gridSize: 32
+        });
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+    myMap.geoObjects.add(objectManager);
+
     $.ajax({
         url: "data.json"
     }).done(function(data) {
         objectManager.add(data);
     });
-});
+  }
+);
 }
